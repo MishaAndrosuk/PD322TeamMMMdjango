1fdca359fb630a7924a71569b82c50d076ada21d
Misha Androshchuk <androsukmisa3@gmail.com>
2025-02-22 20:10:18 +0200

    fix register

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    username = None
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
